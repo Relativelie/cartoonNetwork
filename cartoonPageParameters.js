94486b60fd87dd49099b171b2175a583fd4481b6
Relativelie <sopio1992@gmail.com>
2021-11-22 22:20:56 +0300

    added information about all cartoons

---
+++ cartoonPageParameters.js
@@ -0,0 +1,204 @@
+let cartoonPageItems = {
+    "Adventure Time":
+    {
+        "video": "ATCartoon/ATVideo.gif",
+        "logo": "ATGame/logo.png",
+        "seriesOne": ["ATCartoon/newsImage1.gif", "Come Along with Me", "The fate of Ooo is revealed to two adventurers in the distant future. Princess Bubblegum gathers her allies while Finn searches for a way to prevent the coming Great Gum War."],
+        "seriesTwo": ["ATCartoon/newsImage2.gif", "Gumbaldia", "Finn and Jake, as ambassadors of the Candy Kingdom, travel to a powerful new city-state in a last ditch effort to stave off war."],
+        "seriesThree": ["ATCartoon/newsImageTwo.gif", "Temple of Mars", "Searching for answers on Mars, Finn and Jermaine discover that they must first survive a cunning Martian mental trap."],
+        "about": [
+            "It's one crazy adventure after another for human boy, Finn, and his best friend, Jake, a 28-year old dog with magical powers. They're out to have the most fun possible and they sure do find it exploring the Land of Ooo! Whether it's saving Princess Bubblegum, battling zombie candy, taunting the Ice King or rocking out with Marceline the Vampire Queen, with Finn and Jake it's always ADVENTURE TIME!",
+            "ATCartoon/carouselImage1.jpeg",
+            "ATCartoon/carouselImage2.jpeg",
+            "ATCartoon/carouselImage3.jpeg",
+            "ATCartoon/carouselImage4.jpeg",
+            "ATCartoon/carouselImage5.jpeg",
+            "ATCartoon/carouselImage6.jpeg",
+            "ATCartoon/carouselImage7.jpeg",
+            "ATCartoon/carouselImage8.jpeg",
+            "ATCartoon/picturesNine.jpeg"
+        ],
+        "books": [[
+            "ATCartoon/bookOne.jpeg",
+            "Adventure Time Vol. 1",
+            "The totally algebraic adventures of Finn and Jake have come to the comic book page! The Lich, a super-lame, SUPER-SCARY skeleton dude, has returned to the the Land of Ooo, and he's bent on total destruction! Luckily, Finn and Jake are on the case...but can they succeed against their most destructive foe yet?",
+            "https://www.amazon.com/gp/product/B00U6SVPIM?notRedirectToSDP=1&ref_=dbs_mng_calw_0&storeType=ebooks"
+        ],
+        [
+            "ATCartoon/bookTwo.jpeg",
+            "Adventure Time Vol. 2",
+            "Join Finn the Human, Jake the Dog, and Princess Bubblegum for all-new adventures in The Land of Ooo. Experience the algebraic adventures with your favorite characters. Not only does this graphic novel collect the out-of-control, adventure-packed third story arc, it also includes the unforgettable 'Choose Your Own Adventure' story.",
+            "https://www.amazon.com/gp/product/B00U6SVPPK?notRedirectToSDP=1&ref_=dbs_mng_calw_1&storeType=ebooks"
+        ],
+        [
+            "ATCartoon/bookThree.jpeg",
+            "Adventure Time Vol. 3",
+            "The all-ages hit of the year is back with a new volume collecting even more algebraic adventures! What happens when Jake the dog and Finn the human get a hold of Princess Bubblegum's time machine and travel into the future? Can you say ROBOT FINN & JAKE? Find out more in this exciting volume!",
+            "https://www.amazon.com/gp/product/B00U6SVQ1I?notRedirectToSDP=1&ref_=dbs_mng_calw_2&storeType=ebooks"
+        ]]
+    },
+    "Regular Show":
+    {
+        "video": "regularShow/regularShow.gif",
+        "logo": "regularShow/regularShowLogo.png",
+        "seriesOne": ["regularShow/firstSeries.gif", "A Regular Epic Final Battle", "An epic final battle comes to its dramatic conclusion, and the fate of the universe is decided..."],
+        "seriesTwo": ["regularShow/secondSeries.gif", "Into the Unknown", "Taking place before the events of the first episode, on Halloween, Wirt has just finished making an audio cassette tape for his crush, Sara..."],
+        "seriesThree": ["regularShow/thirdSeries.gif", "Babes in the Wood", "While Wirt and Greg lay down to rest, they're drawn into a dreamworld of cherubs, flowers, and clouds where Greg must battle the North Wind..."],
+        "about": ["Mordecai – a sarcastic blue jay, and Rigby – a somewhat responsible raccoon, are best friends. They even work together at a park owned by Pops, a big lollipop-headed guy. Which seems normal enough. They’ve got a cranky gumball machine for a boss and they work alongside a Zen-like yeti named Skips. Yep. Everything is as regular as can be.",
+            "regularShow/carouselImageRegular.jpeg",
+            "regularShow/carouselImageRegularFive.jpeg",
+            "regularShow/carouselImageRegularOne.jpeg",
+            "regularShow/carouselImageRegularThree.png",
+            "regularShow/carouselImageRegularTwo.jpeg",
+            "regularShow/carouselImageRegularEight.jpeg",
+            "regularShow/carouselImageRegularSix.jpeg",
+            "regularShow/carouselImageRegularseven.png",
+            "regularShow/picturesSeven.jpeg"
+
+        ],
+        "books": [[
+            "regularShow/bookOne.jpeg",
+            "Regular Show: Vol. 1",
+            "It's time for a good dose of cleaning as Benson tries to organize the Park's yearly garage sale...only Rigby is having some trouble letting go. Banished to the garage Rigby comes across a mysterious pogs container that is about to give everyone at the Park a strong dose of the 90's...",
+            "https://www.amazon.com/dp/1785859811/ref=bseries_primary_1_1785859811"
+
+        ],
+        [
+            "regularShow/bookTwo.jpeg",
+            "Regular Show: Vol. 2",
+            "A picture is worth a thousand words, but not in this case! Mordecai and Rigby find themselves in trouble after a selfie war and now they have to destroy all the evidence so no one (especially Margaret) sees Mordecai's scariest picture.",
+            "https://www.amazon.com/dp/B00UBK4FP0/ref=bseries_primary_1_B00UBK4FP0"
+        ],
+        [
+            "regularShow/bookThree.jpeg",
+            "Regular Show: Vol. 4",
+            "It’s the battle of the coffee shops, and it looks like there is no end in sight for this crazy caffeine war. Margaret and Eileen get ready to fight the war but it looks like Mordecai and Rigby might have a plan to help…to hopefully help...",
+            "https://www.amazon.com/Regular-Show-Vol-KC-Green/dp/1608867099/ref=pd_sbs_2/137-3322566-2191325?pd_rd_w=I0PM2&pf_rd_p=3676f086-9496-4fd7-8490-77cf7f43f846&pf_rd_r=E7V9A5T78TYCM0F9EEZQ&pd_rd_r=5d0f8e5d-66ee-4bc6-a571-b7b005ca4053&pd_rd_wg=OveUS&pd_rd_i=1608867099&psc=1"
+        ]]
+    },
+    "Over The Garden Wall":
+    {
+        "video": "gardenWall/overTheGarden.gif",
+        "logo": "gardenWall/wallLogo.png",
+        "seriesOne": ["gardenWall/seriesOne.gif", "The Unknown", "After leaving Wirt with her family, Beatrice navigates through the storm to find Greg, who she sees performing meaningless tasks for the Beast. She is whisked..."],
+        "seriesTwo": ["gardenWall/seriesTwo.gif", "Into the Unknown", "Taking place before the events of the first episode, on Halloween, Wirt has just finished making an audio cassette tape for his crush, Sara..."],
+        "seriesThree": ["gardenWall/seriesThree.gif", "Babes in the Wood", "While Wirt and Greg lay down to rest, they're drawn into a dreamworld of cherubs, flowers, and clouds where Greg must battle the North Wind..."],
+        "about":
+            [
+                "The story features two half-brothers, Wirt and Gregory, who have become lost in a mysterious wood called The Unknown and attempt to find their way home with the help of the Woodsman and Beatrice the bluebird. The atmosphere in The Unknown is generally menacing although there are many moments of playful whimsy. Many of the episodes are rooted in folktales and children’s stories from the 19th and early 20th centuries while the style of animation and storytelling borrow heavily from early cartoons.",
+                "gardenWall/carouselImageOverTheGarden.jpeg",
+                "gardenWall/carouselImageOverTheGardenFive.jpeg",
+                "gardenWall/carouselImageOverTheGardenFour.jpeg",
+                "gardenWall/carouselImageOverTheGardenThree.jpeg",
+                "gardenWall/carouselImageOverTheGardenTwo.png",
+                "gardenWall/picturesSeven.jpeg",
+                "gardenWall/picturesSix.png",
+                "gardenWall/picturesThree.jpeg",
+                "gardenWall/picturesTwo.jpeg"
+            ],
+        "books": [
+            [
+                "gardenWall/bookOneGarden.jpeg",
+                "Hollow Town",
+                "When Wirt seeks work to replace Greg’s lost shoe, Wirt, Greg, and Beatrice stumble upon the seemingly pleasant Hollow Town inhabited by wooden doll people more sinister than they first appear.When Greg loses a shoe, Wirt must find a way to replace it, and fast if they want to avoid a repeat of the summer camp splinter incident.",
+                "https://www.amazon.com/Over-Garden-Wall-Hollow-Town/dp/1684153832/ref=pd_bxgy_img_2/137-3322566-2191325?pd_rd_w=B5d4K&pf_rd_p=c64372fa-c41c-422e-990d-9e034f73989b&pf_rd_r=NZBQR1XYPQXXBZ2DNQER&pd_rd_r=8bb5456a-a5b8-4348-9c73-ef2d79ff1d1c&pd_rd_wg=cqgfO&pd_rd_i=1684153832&psc=1"
+            ],
+            [
+                "gardenWall/bookTwoGarden.jpeg",
+                "Soulful Symphonies",
+                "When a charming young woman offers Greg and Wirt the roles of a lifetime in the play at her family theater, they decide to audition for the production and test out their acting chops - but who exactly are the three stage sisters running the show and where is everyone else in the town?",
+                "https://www.amazon.com/Over-Garden-Wall-Soulful-Symphonies/dp/1684155568/ref=pd_bxgy_img_1/137-3322566-2191325?pd_rd_w=jHDN4&pf_rd_p=c64372fa-c41c-422e-990d-9e034f73989b&pf_rd_r=Z2B4KW9JHZ23M3DZ9895&pd_rd_r=4f88a279-3831-41f4-a616-37802f97e60e&pd_rd_wg=mEKpr&pd_rd_i=1684155568&psc=1"
+            ],
+            [
+                "gardenWall/bookThreeGarden.jpeg",
+                "Benevolent Sisters of Charity",
+                "Wirt and Greg take refuge in a mysterious woodland hospital in this brand new original graphic novel set in the world inspired by the Emmy® Award-winning Cartoon Network animated series, Over The Garden Wall.",
+                "https://www.amazon.com/Over-Garden-Wall-Benevolent-Sisters/dp/1684156203/ref=pd_sbs_2/137-3322566-2191325?pd_rd_w=PcgyU&pf_rd_p=3676f086-9496-4fd7-8490-77cf7f43f846&pf_rd_r=RW3C85QC1CABSB71G3TM&pd_rd_r=7eba7fe3-9330-4015-8f6b-e96121c25322&pd_rd_wg=oRvJH&pd_rd_i=1684156203&psc=1"
+            ]
+        ]
+    },
+    "The Amazing World of Gumball":
+    {
+        "video": "gumball/gumball.gif",
+        "logo": "gumball/gumballLogo.png",
+        "seriesOne": ["gumball/firstSeries.gif", "The Vegging", "Gumball and Darwin have decided to do nothing for a whole day, but sometimes it's much harder to do nothing than something."],
+        "seriesTwo": ["gumball/secondSeries.gif", "The One", "Gumball is surprised by how upset he is when Tobias makes fun of him, and he tries to figure out why he feels Tobias doesn't have the right."],
+        "seriesThree": ["gumball/thirdSeries.gif", "The Cage", "Gumball and Darwin discover that their teacher, Mr. Corneille, has a secret life as a fighter."],
+        "about": [
+            "Gumball’s world is pretty run-of-the-mill. He’s chased around school by a T-Rex. He has a friend named Anton who’s a piece of toast. He’s got a crush on Penny, a peanut with antlers. His dad is a 6’4” bunny. His mom works at a Rainbow Factory. And his brother is a goldfish named Darwin. Yup, everything looks perfectly normal here.",
+            "gumball/carouselFour.jpeg",
+            "gumball/carouselGumboll.jpg",
+            "gumball/carouselGumbollOne.png",
+            "gumball/carouselGumbollThree.jpeg",
+            "gumball/carouselGumbollTwo.jpg",
+            "gumball/picturesFive.jpeg",
+            "gumball/picturesFour.jpeg",
+            "gumball/picturesOne.jpeg",
+            "gumball/picturesSeven.jpeg"
+        ],
+        "books": [
+            [
+                "gumball/bookOne.jpg",
+                "The Amazing World of Gumball Vol. 1",
+                "Gumball, Darwin, and all of their friends take on the world of comics! Gumball thinks he’s the coolest kid on the block, but his little sister Anais begs to differ, and Darwin just wants to keep them all out of trouble. There’s always another adventure to be had in their mixed-up world!",
+                "https://www.amazon.com/Amazing-World-Gumball-Vol/dp/160886488X"
+            ],
+            [
+                "gumball/bookTwo.jpeg",
+                "The Amazing World of Gumball Vol. 4",
+                "When Anais goes to Darwin and Gumball for help in learning about sports to prove she can beat the new exchange student, things get out of hand when the whole school gets involved in a fake sport that her brothers totally -no, really- know how to play.",
+                "https://www.amazon.com/Amazing-World-Gumball-Ogn-Scrimmage/dp/178586646X/ref=pd_sbs_2/131-3574063-8052144?pd_rd_w=yABXj&pf_rd_p=3676f086-9496-4fd7-8490-77cf7f43f846&pf_rd_r=H687KP0FMF0A1FRY6H3P&pd_rd_r=b8c4a28e-345d-45c8-8cb3-799b9f2ead8b&pd_rd_wg=nbhbI&pd_rd_i=178586646X&psc=1"
+            ],
+            [
+                "gumball/bookThree.jpeg",
+                "The Amazing World of Gumball Vol. 2",
+                "There's no such thing as a dull day in Elmore, with four new self-contained stories: Gumball tries to stay out of trouble for once, but the siren song of the Battle of the Bands proves irresistible...a power suit turns out to be a bit too powerful...",
+                "https://www.amazon.com/Amazing-World-Gumball-Vol/dp/1608867935/ref=pd_sbs_1/131-3574063-8052144?pd_rd_w=4PPY7&pf_rd_p=3676f086-9496-4fd7-8490-77cf7f43f846&pf_rd_r=6S7QN2VVT2E00CMMNWW0&pd_rd_r=154ac2bb-ec8e-4ba4-9c37-c66814e05c37&pd_rd_wg=nrEEs&pd_rd_i=1608867935&psc=1"
+            ]
+        ]
+    },
+    "We Bare Bears":
+    {
+        "video": "bears/bears.gif",
+        "logo": "bears/bearsLogo.svg",
+        "seriesOne": ["bears/firstSeries.gif", "Yuri and the Bear", "Baby Ice Bear lives alone in the arctic and meets a mysterious man named Yuri. Together they help each other survive the cold environment."],
+        "seriesTwo": ["bears/secondSeries.gif", "Everyone's Tube", "Through a standard video browsing session online, we get a glimpse of the different kinds of videos the Bears have posted."],
+        "seriesThree": ["bears/thirdSeries.gif", "Grizz Helps", "When his brothers don't need his help for the day, Grizz leaves the cave to assist anyone he can throughout the city."],
+        "about": [
+            "We Bare Bears is a comedy about three bear siblings named Grizzly, Panda and Ice Bear. Each episode follows their awkward attempts at integrating with the human world in the San Francisco Bay Area, whether they're looking for food, trying to make human friends or scheming to become Internet famous. Whatever the situation, it's obvious that being a bear in the civilized, modern world is tough but at least they have each other!",
+            "bears/carouselBear.jpeg",
+            "bears/carouselBearFour.jpeg",
+            "bears/carouselBearOne.jpeg",
+            "bears/carouselBearThree.jpeg",
+            "bears/carouselBearTwo.jpeg",
+            "bears/picturesEight.jpeg",
+            "bears/picturesFive.jpeg",
+            "bears/picturesFour.jpg",
+            "bears/picturesNine.jpeg"
+        ],
+        "books": [
+            [
+                "bears/bookOne.jpeg",
+                "Bears: Awesome at Everything",
+                "Grizz, Panda, and Ice Bear are here to set the record straight about bears. Humans may think bears are slobby, smelly, and lack social graces, but that's not true! Here, the bears of We Bare Bears share what's great about bears in their own bizarre, yet heartfelt way—complete with jazzed-up graphics and fun (mostly fabricated) facts. This 'educational' guide is one fans won't want to miss!",
+                "https://www.amazon.com/Bears-Awesome-Everything-We-Bare/dp/1101996153"
+            ],
+            [
+                "bears/bookTwo.jpeg",
+                "We Go Everywhere Handbook",
+                "The We Go Everywhere Handbook is jammed with games, activities, puzzles, doodles, writing prompts, and more! It's the perfect book for We Bare Bears fans of all ages.",
+                "https://www.amazon.com/We-Bare-Bears-Everywhere-Handbook/dp/1101996145/ref=pd_sbs_1/131-3574063-8052144?pd_rd_w=SH7fS&pf_rd_p=3676f086-9496-4fd7-8490-77cf7f43f846&pf_rd_r=DH5MKWH1VMD2S6RZBHJB&pd_rd_r=a075b8c4-8094-44d1-8092-06f554270623&pd_rd_wg=7aFvs&pd_rd_i=1101996145&psc=1"
+            ],
+            [
+                "bears/bookThree.jpeg",
+                "We Bare Bears Coloring Book",
+                "Remind yourself in the most wonderful time of year on the Holiday season. Wish you all joy! By giving your loves one cute present- this We Bare Bears coloring book: special We Bare Bears coloring book in an adorable style, they can feel warm and meaningful in the Holiday.",
+                "https://www.amazon.com/Bare-Bears-Coloring-Book-Characters/dp/B08KQDYS5L/ref=pd_sbs_3/131-3574063-8052144?pd_rd_w=ukgis&pf_rd_p=3676f086-9496-4fd7-8490-77cf7f43f846&pf_rd_r=6QVK3ZMSTF9FK3NPMPNW&pd_rd_r=5c3b03de-c9a6-4f15-817e-27958fa07a92&pd_rd_wg=MkUvm&pd_rd_i=B08KQDYS5L&psc=1"
+            ]
+        ]
+    }
+}
+
+
+
+export default cartoonPageItems;
---
+++ cartoonScript.js
@@ -0,0 +1,172 @@
+import cartoonPageItems from './cartoonPageParameters.js';
+
+window.addEventListener("resize", mainVideoHeight);
+window.addEventListener('load', mainVideoHeight);
+
+
+function mainVideoHeight() {
+
+    document.querySelector(".mainBackgroundImage").style.top = `-${document.querySelector(".mainBackgroundImage").offsetHeight / 2}px`;
+    document.querySelector(".mainImage").style.marginTop = `${document.querySelector(".mainBackgroundImage").offsetHeight / 4}px`
+}
+
+
+
+// MENU
+let menuOpen = false;
+document.querySelector(".cartoons:nth-child(1)").addEventListener("click", menuDropdown);
+document.addEventListener("keyup", menuDropdownEsc);
+document.addEventListener("click", menuDropdownEsc);
+
+
+function menuDropdown() {
+    if (!(menuOpen)) {
+        document.querySelector(".valueOfCartoons").style.display = "grid";
+        document.querySelector(".menuDropdown img").src = "ATGame/sortDown.png";
+        menuOpen = true;
+    }
+
+    else {
+        document.querySelector(".valueOfCartoons").style.display = "none";
+        document.querySelector(".menuDropdown img").src = "ATGame/sortUp.png";
+        menuOpen = false;
+    }
+
+}
+
+function menuDropdownEsc(e) {
+    if ((e.key === "Escape" && menuOpen) || (menuOpen && e.type === "click" && e.srcElement.classList != "menuDropdown" && e.srcElement.parentElement.classList[0] != "menuDropdown")) {
+        document.querySelector(".valueOfCartoons").style.display = "none";
+        document.querySelector(".menuDropdown img").src = "ATGame/sortUp.png";
+        menuOpen = false;
+    }
+}
+
+
+// about image
+document.querySelector(".back").addEventListener("click", scrollImages);
+document.querySelector(".next").addEventListener("click", scrollImages);
+
+
+let value = 0;
+function scrollImages(e) {
+
+    if (window.matchMedia("(max-width: 500px)").matches) {
+        if (e.srcElement.classList[0] === "back" && value != 0) {
+            value += 22;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+
+        else if (e.srcElement.classList[0] === "next" && value != -88) {
+            value -= 22;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+
+        else if (value === -88) {
+            value = 0;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+    }
+
+    else if (window.matchMedia("(max-width: 1150px)").matches) {
+        if (e.srcElement.classList[0] === "back" && value != 0) {
+            value += 33.333;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+
+        else if (e.srcElement.classList[0] === "next" && value != -66.666) {
+            value -= 33.333;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+
+        else if (value === -66.666) {
+            value = 0;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+    }
+
+
+
+    else {
+        if (e.srcElement.classList[0] === "back" && value != 0) {
+            value += 33.333;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+
+        else if (e.srcElement.classList[0] === "next" && value != -199.998) {
+            value -= 33.333;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+
+        else if (value === -199.998) {
+            value = 0;
+            document.querySelector(".carouselBlock").style.transform = `translateX(${value}%)`;
+        }
+    }
+
+}
+
+
+document.querySelector(".carouselBlock").addEventListener("click", selectImage);
+function selectImage(e) {
+    if (e.srcElement.classList[0] != "carouselBlock") {
+        document.querySelector(".largeImage").src = e.srcElement.attributes[1].nodeValue;
+        document.querySelector(".selectedSmallImage").classList.remove("selectedSmallImage");
+        document.querySelector(`.carouselBlock [src="${e.srcElement.attributes[1].nodeValue}"]`).classList.add("selectedSmallImage");
+    }
+}
+
+// SELECTED PAGE
+
+document.querySelector(".valueOfCartoons > :nth-child(1)").addEventListener("click", selectedPage);
+document.querySelector(".valueOfCartoons > :nth-child(2)").addEventListener("click", selectedPage);
+document.querySelector(".valueOfCartoons > :nth-child(3)").addEventListener("click", selectedPage);
+document.querySelector(".valueOfCartoons > :nth-child(4)").addEventListener("click", selectedPage);
+document.querySelector(".valueOfCartoons > :nth-child(5)").addEventListener("click", selectedPage);
+
+
+function selectedPage(e) {
+
+    document.querySelector(".mainImageBlock img").src = cartoonPageItems[e.srcElement.innerText].video;
+    document.querySelector(".mainBackgroundImage").src = cartoonPageItems[e.srcElement.innerText].video;
+    document.querySelector(".nameOfCartoon img").src = cartoonPageItems[e.srcElement.innerText].logo;
+    document.querySelector(".menuLogo").src = cartoonPageItems[e.srcElement.innerText].logo;
+    let backgroundSeries = document.querySelectorAll(".backgroundSeries");
+    let seriesHeader = document.querySelectorAll(".seriesHeader h3");
+    let seriesPar = document.querySelectorAll(".seriesText p");
+    let backNumber;
+
+    for (let i = 0; i < backgroundSeries.length; i++) {
+        if (i === 0) backNumber = "seriesOne";
+        else if (i === 1) backNumber = "seriesTwo";
+        else backNumber = "seriesThree";
+        backgroundSeries[i].style.backgroundImage = `url("${cartoonPageItems[e.srcElement.innerText][backNumber][0]}")`;
+        seriesHeader[i].textContent = cartoonPageItems[e.srcElement.innerText][backNumber][1];
+        seriesPar[i].textContent = cartoonPageItems[e.srcElement.innerText][backNumber][2];
+    }
+
+    document.querySelector(".aboutCartoonBlock h3").textContent = e.srcElement.innerText;
+    document.querySelector(".aboutCartoonText p").textContent = cartoonPageItems[e.srcElement.innerText].about[0];
+
+    let aboutImages = document.querySelectorAll(".imageCarousel img:not(.imageScrollButtonsBlock img):not(.largeImage)")
+    for (let image = 0; image < aboutImages.length; image++) {
+        aboutImages[image].src = cartoonPageItems[e.srcElement.innerText].about[image + 1];
+    }
+
+    document.querySelector(".largeImage").src = cartoonPageItems[e.srcElement.innerText].about[1]
+
+    let bookHeader = document.querySelectorAll(".page h2");
+    let bookRearPart = document.querySelectorAll(".page-5");
+    let bookBack = document.querySelectorAll(".pageCover");
+    let bookText = document.querySelectorAll(".page p");
+    let bookBuyLink = document.querySelectorAll(".buyBtn a");
+    for (let book = 0; book < bookHeader.length; book++) {
+        bookBack[book].style.backgroundImage = `url("${cartoonPageItems[e.srcElement.innerText].books[book][0]}")`;
+        bookRearPart[book].style.backgroundImage = `url("${cartoonPageItems[e.srcElement.innerText].books[book][0]}")`;
+        bookHeader[book].textContent = cartoonPageItems[e.srcElement.innerText].books[book][1];
+        bookText[book].textContent = cartoonPageItems[e.srcElement.innerText].books[book][2];
+        bookBuyLink[book].href = cartoonPageItems[e.srcElement.innerText].books[book][3];
+
+    }
+
+}
